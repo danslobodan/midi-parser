8f4fe26105942687a28e5eb8cd055266a641ac37
Slobodan Dan <danastroman@gmail.com>
2023-02-22 16:38:34 +0100

    Removed close notes function.

--- src/joinMidi.ts
+++ src/joinMidi.ts
@@ -1,9 +1,18 @@
 import { IMidiFile } from "./domain/MidiFile";
 import { IMidiTrack, TRACK_HEADER_SIGNATURE } from "./domain/MidiTrack";
 import { IMidiEvent, EventType } from "./domain/IMidiEvent";
-import { NoteOn, NoteOff } from "./domain/midi-event";
-import { Pitch } from "./domain/midi-event/midi-component";
 import { IMetaEvent, MetaEventType } from "./domain/meta-events/IMetaEvent";
+
+export const joinMany = (files: IMidiFile[]): IMidiFile => {
+    const tracks: IMidiTrack[] = [];
+
+    for (let i = 1; i < files.length; i++) {}
+
+    return {
+        header: files[0].header,
+        tracks,
+    };
+};
 
 export const joinMidi = (file1: IMidiFile, file2: IMidiFile): IMidiFile => {
     const tracks: IMidiTrack[] = [];
@@ -23,6 +32,18 @@ export const joinMidi = (file1: IMidiFile, file2: IMidiFile): IMidiFile => {
     };
 };
 
+const fileDuration = (file: IMidiFile) => {
+    return file.tracks
+        .map((track) => trackDuration(track))
+        .reduce((result, current) => (current > result ? current : result), 0);
+};
+
+const trackDuration = (track: IMidiTrack) => {
+    return track.events
+        .map((event) => event.deltaTime)
+        .reduce((result, current) => result + current, 0);
+};
+
 const joinTracks = (
     track1: IMidiTrack,
     track2: IMidiTrack,
@@ -31,10 +52,9 @@ const joinTracks = (
     const track1Events = track1.events.slice(0, -1);
     const track2Events = removeRedundantEvents(track2);
 
-    const track1EventsClosed = closeNotes(track1Events);
     track2Events[0].deltaTime += trackOffset;
 
-    const events = [...track1EventsClosed, ...track2Events];
+    const events = [...track1Events, ...track2Events];
     return {
         header: TRACK_HEADER_SIGNATURE.toString(16),
         lengthBytes: track1.lengthBytes + track2.lengthBytes,
@@ -50,48 +70,4 @@ const removeRedundantEvents = (track: IMidiTrack): IMidiEvent[] => {
             event.type !== EventType.PROGRAM_CHANGE &&
             event.type !== EventType.CONTROLLER_CHANGE
     );
-};
-
-const fileDuration = (file: IMidiFile) => {
-    return file.tracks
-        .map((track) => trackDuration(track))
-        .reduce((result, current) => (current > result ? current : result), 0);
-};
-
-const trackDuration = (track: IMidiTrack) => {
-    return track.events
-        .map((event) => event.deltaTime)
-        .reduce((result, current) => result + current, 0);
-};
-
-const closeNotes = (events: IMidiEvent[]): IMidiEvent[] => {
-    const openNotes = new Map<number, NoteOn>();
-    for (let i = 0; i < events.length; i++) {
-        if (events[i].type === EventType.NOTE_ON) {
-            const noteOn = events[i] as NoteOn;
-            openNotes.set(noteOn.pitch.Value(), noteOn);
-        }
-        if (events[i].type === EventType.NOTE_OFF) {
-            const noteOff = events[i] as NoteOff;
-            openNotes.delete(noteOff.pitch.Value());
-        }
-    }
-
-    const RELEASE_VELOCITY = 64;
-    let runningStatus = false;
-    const closed = [...openNotes.values()].flatMap((noteOn) => {
-        const noteOff = new NoteOff(
-            0,
-            noteOn.channel,
-            noteOn.pitch.Value(),
-            RELEASE_VELOCITY,
-            runningStatus
-        );
-
-        if (!runningStatus) runningStatus = true;
-
-        return noteOff;
-    });
-
-    return [...events, ...closed];
 };
